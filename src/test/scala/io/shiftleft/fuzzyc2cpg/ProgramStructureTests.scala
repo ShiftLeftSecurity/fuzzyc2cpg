13ba6f86370d6df8babe360dc83a60a0072b0a1e
Fabian Yamaguchi <fabs@shiftleft.io>
2018-10-05 19:07:06 +0200

    Check for global namespace node.

--- src/test/scala/io/shiftleft/fuzzyc2cpg/ProgramStructureTests.scala
+++ src/test/scala/io/shiftleft/fuzzyc2cpg/ProgramStructureTests.scala
@@ -8,12 +8,22 @@ class ProgramStructureTests extends WordSpec with Matchers {
   val g = fixture.cpg.scalaGraph.traversal
 
   "Program structure of test project" should {
+
+    "contain <global> namespace node" in {
+      val namespaceBlock =
+        g.V.hasLabel(NodeType.NAMESPACE_BLOCK.toString).headOption
+      namespaceBlock.isDefined shouldBe true
+      val name = namespaceBlock.get.property[String]("NAME").value
+      name shouldBe "<global>"
+    }
+
     "contain one file node" in {
       val fileNode = g.V.hasLabel(NodeType.FILE.toString).headOption
       fileNode.isDefined shouldBe true
-      val fileName = fileNode.get.property[String]("NAME").value()
+      val fileName = fileNode.get.property[String]("NAME").value
       fileName shouldBe "src/test/resources/testcode/structure/structure.c"
     }
+
   }
 
 }
