84cba7a0e938916b45513e8e7dec05726bdab9bf
Markus Lottmann <markus@shiftleft.io>
2018-10-23 15:07:49 +0200

    Rearrange code to not have nested functions.

--- src/main/scala/io/shiftleft/fuzzyc2cpg/MethodCreator.scala
+++ src/main/scala/io/shiftleft/fuzzyc2cpg/MethodCreator.scala
@@ -14,109 +14,111 @@ import io.shiftleft.proto.cpg.Cpg.{CpgStruct, NodePropertyName, PropertyValue}
 import io.shiftleft.fuzzyc2cpg.Utils.{children, newStringProperty}
 import io.shiftleft.proto.cpg.Cpg.CpgStruct.Edge.EdgeType
 
-import scala.collection.mutable
 import scala.collection.JavaConverters._
 
-class MethodCreator(structureCpg: StructureCpg) {
+class MethodCreator(structureCpg: StructureCpg,
+                    functionDef: FunctionDefBase) {
+  private var nodeToProtoNode = Map[CfgNode, CpgStruct.Node]()
+  private val bodyCpg = CpgStruct.newBuilder()
+  private var methodNode: CpgStruct.Node = _
+  val cfg = initializeCfg(functionDef)
 
-  def addMethodBodyCpg(methodNode : Node,
-                               ast: FunctionDefBase) : CpgStruct.Builder = {
+  def addMethodCpg(): CpgStruct.Builder = {
+    methodNode = addMethodStubToStructureCpg()
+    addMethodBodyCpg()
+  }
 
-    val nodeToProtoNode: mutable.Map[CfgNode, CpgStruct.Node] =
-      new mutable.HashMap[CfgNode, CpgStruct.Node]
-    val bodyCpg = CpgStruct.newBuilder()
-    val cfg = initializeCfg(ast)
+  def addMethodBodyCpg() : CpgStruct.Builder = {
 
-
-    def addNewTrueLiteralNode(cfgNode : CfgNode): Unit = {
-      val codeProperty = Node.Property.newBuilder.setName(NodePropertyName.NAME)
-        .setValue(PropertyValue.newBuilder.setStringValue("<true>").build)
-        .build
-      val nodeBuilder = Node.newBuilder.setType(NodeType.LITERAL).addProperty(codeProperty)
-      nodeToProtoNode.put(cfgNode, nodeBuilder.build)
-      bodyCpg.addNode(nodeBuilder)
-    }
-
-    def addAllNodesOfExpression (expression: Expression) {
-      addNodeForExpressionRoot(expression)
-      children(expression).foreach{child =>
-        addAllNodesOfExpression(child.asInstanceOf[Expression])
-      }
-    }
-
-    def addNodeForExpressionRoot(expression: Expression): Unit = {
-      val nodeBuilder = Node.newBuilder
-      if (expression.isInstanceOf[CallExpression]) {
-        val callExpression = expression.asInstanceOf[CallExpression]
-        val targetFunc = callExpression.getTargetFunc
-        val operator = targetFunc.getEscapedCodeStr
-        nodeBuilder.addProperty(newStringProperty(NodePropertyName.NAME, operator))
-      }
-      bodyCpg.addNode(nodeBuilder)
-    }
-
-    def addStatementNodes(cfgNode: CfgNode): Unit = {
-      assert(cfgNode.isInstanceOf[ASTNodeContainer])
-      val container = cfgNode.asInstanceOf[ASTNodeContainer]
-      val astNode = container.getASTNode
-
-      astNode match {
-        case parameter: Parameter => {}
-        case exprStmt : ExpressionStatement => {
-          val expression = exprStmt.getExpression
-          addAllNodesOfExpression(expression)
-        }
-        case condition : Condition => {
-          addAllNodesOfExpression(condition.getExpression)
-        }
-        case stmt: IdentifierDeclStatement => {
-          stmt.getIdentifierDeclList.asScala.foreach{ node =>
-            children(node).filter(_.isInstanceOf[AssignmentExpression])
-            .foreach { child =>
-              addAllNodesOfExpression(child.asInstanceOf[Expression])
-            }
-          }
-        }
-        case _ => println("Unhandled node type: " + astNode.getClass.getSimpleName)
-      }
-    }
-
-    def addNodes: Unit = {
-      for (cfgNode <- cfg.getVertices.asScala) {
-        if (cfgNode.isInstanceOf[CfgEntryNode]) {
-          // No need to add the start node. The start nodes
-          // corresponds to the method node in the CPG, and
-          // that's already present in the structureCpg.
-          // However, we do need to add it to the `nodeToProtoNode`
-          // map so that the node is present when creating edges
-          nodeToProtoNode.put(cfg.getEntryNode, methodNode)
-        }
-        else if (cfgNode.isInstanceOf[CfgErrorNode] ||
-          cfgNode.isInstanceOf[CfgExceptionNode] ||
-          cfgNode.isInstanceOf[CfgExitNode] ||
-          cfgNode.isInstanceOf[InfiniteForNode])
-          addNewTrueLiteralNode(cfgNode)
-        else if (cfgNode.isInstanceOf[ASTNodeContainer])
-          addStatementNodes(cfgNode)
-      }
-    }
-
-    def addEdges : Unit = {
-
-    }
-
-    addNodes
-    addEdges
+    addBodyNodes
+    addBodyEdges
     bodyCpg
   }
 
-  private def initializeCfg(ast: FunctionDefBase) : CFG = {
+  def addNewTrueLiteralNode(cfgNode : CfgNode): Unit = {
+    val codeProperty = Node.Property.newBuilder.setName(NodePropertyName.NAME)
+      .setValue(PropertyValue.newBuilder.setStringValue("<true>").build)
+      .build
+    val nodeBuilder = Node.newBuilder.setType(NodeType.LITERAL).addProperty(codeProperty)
+    nodeToProtoNode += (cfgNode -> nodeBuilder.build)
+    bodyCpg.addNode(nodeBuilder)
+  }
+
+  def addAllNodesOfExpression (expression: Expression) {
+    addNodeForExpressionRoot(expression)
+    children(expression).foreach{child =>
+      addAllNodesOfExpression(child.asInstanceOf[Expression])
+    }
+  }
+
+  def addNodeForExpressionRoot(expression: Expression): Unit = {
+    val nodeBuilder = Node.newBuilder
+    if (expression.isInstanceOf[CallExpression]) {
+      val callExpression = expression.asInstanceOf[CallExpression]
+      val targetFunc = callExpression.getTargetFunc
+      val operator = targetFunc.getEscapedCodeStr
+      nodeBuilder.addProperty(newStringProperty(NodePropertyName.NAME, operator))
+    }
+    bodyCpg.addNode(nodeBuilder)
+  }
+
+  def addStatementNodes(cfgNode: CfgNode): Unit = {
+    assert(cfgNode.isInstanceOf[ASTNodeContainer])
+    val container = cfgNode.asInstanceOf[ASTNodeContainer]
+    val astNode = container.getASTNode
+
+    astNode match {
+      case parameter: Parameter => {}
+      case exprStmt : ExpressionStatement => {
+        val expression = exprStmt.getExpression
+        addAllNodesOfExpression(expression)
+      }
+      case condition : Condition => {
+        addAllNodesOfExpression(condition.getExpression)
+      }
+      case stmt: IdentifierDeclStatement => {
+        stmt.getIdentifierDeclList.asScala.foreach{ node =>
+          children(node).filter(_.isInstanceOf[AssignmentExpression])
+            .foreach { child =>
+              addAllNodesOfExpression(child.asInstanceOf[Expression])
+            }
+        }
+      }
+      case _ => println("Unhandled node type: " + astNode.getClass.getSimpleName)
+    }
+  }
+
+  def addBodyNodes: Unit = {
+    for (cfgNode <- cfg.getVertices.asScala) {
+      if (cfgNode.isInstanceOf[CfgEntryNode]) {
+        // No need to add the start node. The start nodes
+        // corresponds to the method node in the CPG, and
+        // that's already present in the structureCpg.
+        // However, we do need to add it to the `nodeToProtoNode`
+        // map so that the node is present when creating edges
+        nodeToProtoNode += (cfg.getEntryNode -> methodNode)
+      }
+      else if (cfgNode.isInstanceOf[CfgErrorNode] ||
+        cfgNode.isInstanceOf[CfgExceptionNode] ||
+        cfgNode.isInstanceOf[CfgExitNode] ||
+        cfgNode.isInstanceOf[InfiniteForNode])
+        addNewTrueLiteralNode(cfgNode)
+      else if (cfgNode.isInstanceOf[ASTNodeContainer])
+        addStatementNodes(cfgNode)
+    }
+  }
+
+  def addBodyEdges : Unit = {
+
+  }
+
+  private def initializeCfg(ast: FunctionDefBase): CFG = {
     val converter = new ASTToCFGConverter
     converter.setFactory(new CCFGFactory)
     converter.convert(ast)
   }
 
-  def addMethodStubToStructureCpg(functionDef: FunctionDefBase) = {
+  def addMethodStubToStructureCpg(): CpgStruct.Node = {
     val name = functionDef.getName
     val methodNode = Node.newBuilder.setKey(IdPool.getNextId)
       .setType(NodeType.METHOD)
