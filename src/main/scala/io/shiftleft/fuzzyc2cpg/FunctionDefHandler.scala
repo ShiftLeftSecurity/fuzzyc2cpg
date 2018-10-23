77caa506f531a0ea690a5ffe4688c7167f3bb1ef
Markus Lottmann <markus@shiftleft.io>
2018-10-23 13:00:45 +0200

    Merge method stub and method body creator.

--- src/main/scala/io/shiftleft/fuzzyc2cpg/FunctionDefHandler.scala
+++ src/main/scala/io/shiftleft/fuzzyc2cpg/FunctionDefHandler.scala
@@ -7,13 +7,12 @@ import io.shiftleft.fuzzyc2cpg.outputmodules.OutputModule
 
 class FunctionDefHandler(structureCpg: StructureCpg, outputModule: OutputModule) {
 
-  val stubBuilder = new MethodStubCreator(structureCpg)
-  val bodyCreator = new MethodBodyCreator()
+  val methodCreator = new MethodCreator(structureCpg)
 
   def handle(ast: FunctionDefBase): Unit = {
 
-    val methodNode = stubBuilder.addMethodStubToStructureCpg(ast)
-    val bodyCpg = bodyCreator.addMethodBodyCpg(methodNode, ast)
+    val methodNode = methodCreator.addMethodStubToStructureCpg(ast)
+    val bodyCpg = methodCreator.addMethodBodyCpg(methodNode, ast)
     val outputFilename = generateOutputFilename(ast)
     outputModule.output(bodyCpg, outputFilename)
   }
--- src/main/scala/io/shiftleft/fuzzyc2cpg/MethodCreator.scala
+++ src/main/scala/io/shiftleft/fuzzyc2cpg/MethodCreator.scala
@@ -1,7 +1,7 @@
 package io.shiftleft.fuzzyc2cpg
 
 import io.shiftleft.fuzzyc2cpg.ast.expressions.{AssignmentExpression, Expression}
-import io.shiftleft.fuzzyc2cpg.ast.functionDef.FunctionDefBase
+import io.shiftleft.fuzzyc2cpg.ast.functionDef.{FunctionDefBase, ParameterBase, ReturnType}
 import io.shiftleft.fuzzyc2cpg.ast.langc.expressions.CallExpression
 import io.shiftleft.fuzzyc2cpg.ast.langc.functiondef.Parameter
 import io.shiftleft.fuzzyc2cpg.ast.logical.statements.Condition
@@ -9,14 +9,15 @@ import io.shiftleft.fuzzyc2cpg.ast.statements.{ExpressionStatement, IdentifierDe
 import io.shiftleft.fuzzyc2cpg.cfg.{ASTToCFGConverter, CCFGFactory, CFG}
 import io.shiftleft.fuzzyc2cpg.cfg.nodes._
 import io.shiftleft.proto.cpg.Cpg.CpgStruct.Node
-import io.shiftleft.proto.cpg.Cpg.CpgStruct.Node.NodeType
+import io.shiftleft.proto.cpg.Cpg.CpgStruct.Node.{NodeType, Property}
 import io.shiftleft.proto.cpg.Cpg.{CpgStruct, NodePropertyName, PropertyValue}
 import io.shiftleft.fuzzyc2cpg.Utils.{children, newStringProperty}
+import io.shiftleft.proto.cpg.Cpg.CpgStruct.Edge.EdgeType
 
 import scala.collection.mutable
 import scala.collection.JavaConverters._
 
-class MethodBodyCreator() {
+class MethodCreator(structureCpg: StructureCpg) {
 
   def addMethodBodyCpg(methodNode : Node,
                                ast: FunctionDefBase) : CpgStruct.Builder = {
@@ -113,6 +114,65 @@ class MethodBodyCreator() {
     val converter = new ASTToCFGConverter
     converter.setFactory(new CCFGFactory)
     converter.convert(ast)
+  }
+
+  def addMethodStubToStructureCpg(functionDef: FunctionDefBase) = {
+    val name = functionDef.getName
+    val methodNode = Node.newBuilder.setKey(IdPool.getNextId)
+      .setType(NodeType.METHOD)
+      .addProperty(newStringProperty(NodePropertyName.NAME, name))
+      .addProperty(newStringProperty(NodePropertyName.FULL_NAME, name)).build
+
+    structureCpg.addNode(methodNode)
+    connectMethodToNamespaceAndType(methodNode)
+    functionDef.getParameterList.asScala.foreach{ parameter =>
+      addParameterCpg(parameter)
+    }
+
+    val retNode = children(functionDef).find(_.isInstanceOf[ReturnType])
+    val retType = retNode.map(_.getEscapedCodeStr).getOrElse("")
+
+    val methodReturnNode = Node.newBuilder.setKey(IdPool.getNextId)
+      .setType(NodeType.METHOD_RETURN)
+      .addProperty(newStringProperty(NodePropertyName.CODE, "RET"))
+      .addProperty(newStringProperty(NodePropertyName.TYPE_FULL_NAME, retType ))
+      .build
+    structureCpg.addNode(methodReturnNode)
+
+    methodNode
+  }
+
+  private def connectMethodToNamespaceAndType(methodNode: CpgStruct.Node): Unit = {
+    structureCpg.addEdge(
+      CpgStruct.Edge.newBuilder.setType(EdgeType.AST)
+        .setSrc(structureCpg.getNamespaceBlockNode.getKey)
+        .setDst(methodNode.getKey).build)
+  }
+
+  private def addParameterCpg(parameter: ParameterBase): Unit = {
+    val codeProperty = newStringProperty(NodePropertyName.CODE, parameter.getEscapedCodeStr)
+    val nameProperty = newStringProperty(NodePropertyName.NAME, parameter.getName)
+    val orderProperty = Property.newBuilder
+      .setName(NodePropertyName.ORDER)
+      .setValue(PropertyValue.newBuilder.setIntValue(parameter.getChildNumber))
+      .build
+
+    structureCpg.addNode(
+      Node.newBuilder.setKey(IdPool.getNextId)
+        .setType(NodeType.METHOD_PARAMETER_IN)
+        .addProperty(codeProperty)
+        .addProperty(nameProperty)
+        .addProperty(orderProperty).build
+    )
+
+    val evalNode = Node.newBuilder
+      .setType(NodeType.TYPE)
+      .addProperty(Property.newBuilder.setName(NodePropertyName.NAME)
+        .setValue(PropertyValue.newBuilder.setStringValue(parameter.getType.getEscapedCodeStr)))
+      .addProperty(Property.newBuilder.setName(NodePropertyName.FULL_NAME)
+        .setValue(PropertyValue.newBuilder.setStringValue(parameter.getType.getEscapedCodeStr)))
+      .build
+    structureCpg.addNode(evalNode)
   }
 
 }
