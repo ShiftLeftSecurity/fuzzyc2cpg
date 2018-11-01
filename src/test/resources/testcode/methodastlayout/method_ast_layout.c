9574c9b925552184347f058e29e3788213f45bf5
Markus Lottmann <markus@shiftleft.io>
2018-11-01 18:14:55 +0100

    Changed ast child number calculation and added tests.

---
+++ src/test/resources/testcode/methodastlayout/method_ast_layout.c
@@ -0,0 +1,22 @@
+void method1() {
+
+}
+
+void method2() {
+  int local = 1;
+}
+
+void method3() {
+  int x;
+  int y;
+  int z;
+
+  x = y + z;
+}
+
+void method4() {
+  int x;
+  {
+    int y;
+  }
+}
