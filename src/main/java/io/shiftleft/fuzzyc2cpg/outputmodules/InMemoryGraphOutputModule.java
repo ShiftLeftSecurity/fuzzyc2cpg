2890202e87573bfa7fd13b5d5e9bdb4ba509a031
Markus Lottmann <markus@shiftleft.io>
2018-11-01 16:17:47 +0100

    Quick fix to be able to write tests for now.
    We will need a proper output module eventually.

--- src/main/java/io/shiftleft/fuzzyc2cpg/outputmodules/InMemoryGraphOutputModule.java
+++ src/main/java/io/shiftleft/fuzzyc2cpg/outputmodules/InMemoryGraphOutputModule.java
@@ -1,35 +1,47 @@
 package io.shiftleft.fuzzyc2cpg.outputmodules;
 
 import io.shiftleft.cpgloading.tinkergraph.ProtoToCpg;
-import io.shiftleft.proto.cpg.Cpg.CpgStruct.Builder;
+import io.shiftleft.proto.cpg.Cpg.CpgStruct;
 import io.shiftleft.cpgloading.ProtoCpgLoader;
 import io.shiftleft.queryprimitives.steps.starters.Cpg;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Output module that writes CPG into in memory graph database
  * */
 public class InMemoryGraphOutputModule implements OutputModule {
 
-  Cpg cpg;
+  private final List<CpgStruct.Builder> cpgBuilders = new ArrayList<>();
 
   @Override
-  public void output(Builder cpgBuilder, String outputFilename) {
+  public void output(CpgStruct.Builder cpgBuilder, String outputFilename) {
+    cpgBuilders.add(cpgBuilder);
+  }
+
+  public Cpg getCpg() {
+    CpgStruct.Builder mergedBuilder = CpgStruct.newBuilder();
+    Cpg cpg;
+
+    cpgBuilders.forEach(builder -> {
+      mergedBuilder.mergeFrom(builder.build());
+    });
+
     ProtoToCpg protoToCpg = new ProtoToCpg();
     ProtoCpgLoader protoCpgLoader = new ProtoCpgLoader(protoToCpg);
 
-    byte[] bytes = cpgBuilder.build().toByteArray();
+    byte[] bytes = mergedBuilder.build().toByteArray();
     InputStream inputStream = new ByteArrayInputStream(bytes);
     try {
       cpg = protoCpgLoader.loadFromInputStream(inputStream);
     } catch (IOException e) {
       System.err.println("Error loading CPG from byte array input stream");
+      cpg = null;
     }
-  }
 
-  public Cpg getCpg() {
     return cpg;
   }
 
