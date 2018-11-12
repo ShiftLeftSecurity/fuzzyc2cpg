27946dc763869bd60043e1b9916694d606f48d9e
Markus Lottmann <markus@shiftleft.io>
2018-11-12 12:02:07 +0100

    The arguments field of ArgumentList has not been used -> removed.

--- src/main/java/io/shiftleft/fuzzyc2cpg/ast/expressions/ArgumentList.java
+++ src/main/java/io/shiftleft/fuzzyc2cpg/ast/expressions/ArgumentList.java
@@ -6,27 +6,7 @@ import io.shiftleft.fuzzyc2cpg.ast.walking.ASTNodeVisitor;
 import java.util.Iterator;
 import java.util.LinkedList;
 
-public class ArgumentList extends ExpressionHolder implements Iterable<Expression> {
-
-  private LinkedList<Expression> arguments = new LinkedList<Expression>();
-
-  public int size() {
-    return this.arguments.size();
-  }
-
-  public Expression getArgument(int index) {
-    return this.arguments.get(index);
-  }
-
-  public void addArgument(Expression argument) {
-    this.arguments.add(argument);
-    super.addChild(argument);
-  }
-
-  @Override
-  public Iterator<Expression> iterator() {
-    return this.arguments.iterator();
-  }
+public class ArgumentList extends ExpressionHolder {
 
   @Override
   public void accept(ASTNodeVisitor visitor) {
