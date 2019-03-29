6f41cc0b9f2230b18412b6e5e68e5125d98840c3
niko <niko@shiftleft.io>
2019-03-29 12:26:02 +0100

    MISRA 2004 test cases

---
+++ src/test/resources/testcode/misra2004/misra_5.3.c
@@ -0,0 +1,9 @@
+{
+    typedef unsigned char uint8_t;
+}
+{
+    typedef unsigned char uint8_t; /* Not compliant - redefinition */
+}
+{
+    unsigned char uint8_t; /* Not compliant - reuse of uint8_t */
+}
