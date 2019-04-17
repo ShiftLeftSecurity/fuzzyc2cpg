ec7f28d2893c8c4378f222bec947e95c558217da
niko <niko@shiftleft.io>
2019-04-17 19:11:48 +0200

    Refactored test case

--- src/test/java/io/shiftleft/fuzzyc2cpg/antlrparsers/moduleparser/FunctionParameterTests.java
+++ src/test/java/io/shiftleft/fuzzyc2cpg/antlrparsers/moduleparser/FunctionParameterTests.java
@@ -72,11 +72,64 @@ public class FunctionParameterTests extends FunctionDefinitionTests
 	}
 	@Test
 	public void testVoidConstArgs() {
-		String input = "static void * black_box(void * const args) {}";
+		String input = "#include <string.h>\n" +
+				"#include <pthread.h>\n" +
+				"#include <stdlib.h>\n" +
+				"#include <unistd.h>\n" +
+				"#include <stdint.h>\n" +
+				"\n" +
+				"/* Basic types disallowed. */\n" +
+				"typedef signed int SINT_32;\n" +
+				"/* Basic types disallowed. */\n" +
+				"typedef unsigned int UINT_32;\n" +
+				"\n" +
+				"/* Prototypes always required. */\n" +
+				"SINT_32 pthread_create(\n" +
+				"    pthread_t * thread,\n" +
+				"    const pthread_attr_t * attr,\n" +
+				"    void * (*start_routine) (void *),\n" +
+				"    void * arg);\n" +
+				"/* Prototypes always required. */\n" +
+				"SINT_32 pthread_join(\n" +
+				"    pthread_t thread,\n" +
+				"    void ** value_ptr);\n" +
+				"/* Prototypes always required. */\n" +
+				"UINT_32 sleep(UINT_32 seconds);\n" +
+				"\n" +
+				"/* Example data, arbitrary. */\n" +
+				"typedef struct { int32_t tux; int32_t baz; int32_t bar; } example_s;\n" +
+				"\n" +
+				"/* Corrupts input. */\n" +
+				"static void * black_box(void * const args)\n" +
+				"{\n" +
+				"    /* Reference to resource shared across thread_count. */\n" +
+				"    example_s * alias = (example_s*)args;\n" +
+				"\n" +
+				"    /* Arbitrary bound. */\n" +
+				"    while ((alias != NULL) && (alias->tux < 10))\n" +
+				"    {\n" +
+				"        /* \"Think\" to simulate some ammount of work. */\n" +
+				"        (void)sleep(0);\n" +
+				"\n" +
+				"        /* If this occurs another thread has changed the shared resource. */\n" +
+				"        if (alias->tux >= 10)\n" +
+				"        {\n" +
+				"            alias += 2048;\n" +
+				"        }\n" +
+				"        else\n" +
+				"        {\n" +
+				"            /* Increment potentially corrupted alias. */\n" +
+				"            alias->tux += 1;\n" +
+				"        }\n" +
+				"    }\n" +
+				"\n" +
+				"    /* Can't return NULL. */\n" +
+				"    return (int32_t*)0;\n" +
+				"}";
 
 		ModuleParser parser = createParser(input);
 		String output = parser.function_def().toStringTree(parser);
-		System.out.println("OKOKOKOK"+output);
+
 		assertTrue(output.startsWith("(function_def"));
 	}
 }
