663fe206c61faf16e98f64b030b4ec36f435fe06
Fabian Yamaguchi <fabs@shiftleft.io>
2019-05-07 09:21:57 +0200

    Add scalafmt + code formatting.

--- src/main/scala/io/shiftleft/fuzzyc2cpg/FuzzyC2Cpg.scala
+++ src/main/scala/io/shiftleft/fuzzyc2cpg/FuzzyC2Cpg.scala
@@ -9,10 +9,10 @@ object FuzzyC2Cpg extends App {
 
   private val logger = LoggerFactory.getLogger(getClass)
 
-  parseConfig.map{ config =>
+  parseConfig.map { config =>
     try {
-      val fuzzyc2Cpg = new Fuzzyc2Cpg(new OutputModuleFactory(config.outputPath,
-        true, false))
+      val fuzzyc2Cpg =
+        new Fuzzyc2Cpg(new OutputModuleFactory(config.outputPath, true, false))
       fuzzyc2Cpg.runAndOutput(config.inputPaths.toArray)
     } catch {
       case exception: Exception =>
@@ -22,17 +22,16 @@ object FuzzyC2Cpg extends App {
     System.exit(0)
   }
 
-  case class Config(inputPaths : Seq[String],
-                    outputPath : String)
-  def parseConfig : Option[Config] =
+  case class Config(inputPaths: Seq[String], outputPath: String)
+  def parseConfig: Option[Config] =
     new scopt.OptionParser[Config](getClass.getSimpleName) {
       arg[String]("<input-dir>")
         .unbounded()
         .text("source directories containing C/C++ code")
-        .action((x,c) => c.copy(inputPaths = c.inputPaths :+ x))
+        .action((x, c) => c.copy(inputPaths = c.inputPaths :+ x))
       opt[String]("out")
         .text("output filename")
-        .action((x,c) => c.copy(outputPath = x))
+        .action((x, c) => c.copy(outputPath = x))
 
     }.parse(args, Config(List(), DEFAULT_CPG_OUT_FILE))
 
--- src/main/scala/io/shiftleft/fuzzyc2cpg/scope/ScopeElement.scala
+++ src/main/scala/io/shiftleft/fuzzyc2cpg/scope/ScopeElement.scala
@@ -6,8 +6,7 @@ package io.shiftleft.fuzzyc2cpg.scope
   * @tparam V Variable type.
   * @tparam S Scope type.
   */
-case class ScopeElement[I, V, S](scopeNode: S,
-                                 variables: Map[I, V] = Map[I, V]()) {
+case class ScopeElement[I, V, S](scopeNode: S, variables: Map[I, V] = Map[I, V]()) {
   def addVariable(identifier: I, variable: V): ScopeElement[I, V, S] = {
     ScopeElement(scopeNode, variables + (identifier -> variable))
   }
