89426de554bb561d34c971a350a7a403fb414695
Fabian Yamaguchi <fabs@shiftleft.io>
2019-07-11 08:01:56 +0200

    scalafmt.

--- src/main/scala/io/shiftleft/fuzzyc2cpg/SourceFiles.scala
+++ src/main/scala/io/shiftleft/fuzzyc2cpg/SourceFiles.scala
@@ -8,9 +8,9 @@ object SourceFiles {
     * For a given array of input paths, determine all C/C++
     * source files by inspecting filename extensions.
     * */
-  def determine(inputPaths : List[String])  : List[String]= {
+  def determine(inputPaths: List[String]): List[String] = {
 
-    def hasSourceFileExtension(file : File) : Boolean = {
+    def hasSourceFileExtension(file: File): Boolean = {
       val ext = file.extension
       ext.contains(".c") || ext.contains(".cpp") || ext.contains(".h") || ext.contains(".hpp")
     }
@@ -18,8 +18,9 @@ object SourceFiles {
     val (dirs, files) = inputPaths.partition(File(_).isDirectory)
 
     val matchingFiles = files.filter(f => hasSourceFileExtension(File(f)))
-    val matchingFilesFromDirs = dirs.flatMap(dir => File(dir).listRecursively.filter(hasSourceFileExtension))
-        .map(File(".").path.relativize(_).toString)
+    val matchingFilesFromDirs = dirs
+      .flatMap(dir => File(dir).listRecursively.filter(hasSourceFileExtension))
+      .map(File(".").path.relativize(_).toString)
 
     matchingFiles ++ matchingFilesFromDirs
   }
