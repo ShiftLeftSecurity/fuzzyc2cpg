c041915ee6a8b1c3832f5405e7f53388fa67ff5e
Fabian Yamaguchi <fabs@shiftleft.io>
2019-10-24 17:48:05 +0200

    Address review.

--- src/main/scala/io/shiftleft/fuzzyc2cpg/parser/CodeLocationExtractor.scala
+++ src/main/scala/io/shiftleft/fuzzyc2cpg/parser/CodeLocationExtractor.scala
@@ -9,10 +9,10 @@ object CodeLocationExtractor {
     val startLine = Some(ctx.start.getLine)
     val startPos = Some(ctx.start.getCharPositionInLine)
     val startIndex = Some(ctx.start.getStartIndex)
-    val endLine = if (ctx.stop != null) { Some(ctx.stop.getLine) } else { None }
-    val endIndex = if (ctx.stop != null) { Some(ctx.stop.getStopIndex) } else { None }
-    val endPos = if (ctx.stop != null) { Some(ctx.stop.getCharPositionInLine) } else { None }
-    new CodeLocation(startLine, startPos, startIndex, endIndex, endLine, endPos)
+    val endLine = Option(ctx.stop).map(_.getLine)
+    val endIndex = Option(ctx.stop).map(_.getStopIndex)
+    val endPos = Option(ctx.stop).map(_.getCharPositionInLine)
+    CodeLocation(startLine, startPos, startIndex, endIndex, endLine, endPos)
   }
 
 }
