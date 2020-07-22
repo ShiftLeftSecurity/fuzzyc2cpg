32d4f04c66368eed7be75853c85fd5621ee9c85a
Fabian Yamaguchi <fabs@shiftleft.io>
2020-07-22 17:31:21 +0200

    MetaData and AstCreation Passes (#210)
    * Passes up to and including AST creation - minus most tests
    
    * Patches ported over from `fabs/simplify` for `for` handling
    
    Also places method return as last child of method again like
    it was in the old converter.
    
    * Adapt test to fix
    
    * Port over more tests
    
    * Ported over all AST tests
    
    * Replace `println` with call to logger
    
    * Cleanup
    
    * More cleanup
    
    * More cleanup
    
    * Fix typo
    
    * Some comments
    
    * Merge FileAndNamespaceBlockPass and AstCreatorPass
    
    * Perform id assignment in AstCreationPass
    
    * Set correct CPG version

--- build.sbt
+++ build.sbt
@@ -3,7 +3,7 @@ organization := "io.shiftleft"
 scalaVersion := "2.13.1"
 enablePlugins(GitVersioning)
 
-val cpgVersion = "0.11.329"
+val cpgVersion = "0.11.331"
 val antlrVersion = "4.7.2"
 
 libraryDependencies ++= Seq(
@@ -11,6 +11,7 @@ libraryDependencies ++= Seq(
   "org.antlr"            %  "antlr4-runtime"           % antlrVersion,
   "io.shiftleft"         %% "codepropertygraph"        % cpgVersion,
   "io.shiftleft"         %% "codepropertygraph-protos" % cpgVersion,
+  "io.shiftleft"         %% "semanticcpg"              % cpgVersion,
 
   "commons-cli"          %  "commons-cli"              % "1.4",
   "com.github.pathikrit" %% "better-files"             % "3.8.0",
---
+++ src/main/scala/io/shiftleft/fuzzyc2cpg/passes/AstCreationPass.scala
@@ -0,0 +1,70 @@
+package io.shiftleft.fuzzyc2cpg.passes
+
+import io.shiftleft.codepropertygraph.Cpg
+import io.shiftleft.codepropertygraph.generated.{EdgeTypes, nodes}
+import io.shiftleft.fuzzyc2cpg.Utils.getGlobalNamespaceBlockFullName
+import io.shiftleft.fuzzyc2cpg.passes.astcreation.{AntlrCModuleParserDriver, AstVisitor}
+import io.shiftleft.fuzzyc2cpg.{Defines, Global}
+import io.shiftleft.passes.{DiffGraph, IntervalKeyPool, KeyPool, ParallelCpgPass}
+import org.slf4j.LoggerFactory
+
+/**
+  * Given a list of filenames, this pass creates abstract syntax trees for
+  * each file, including File and NamespaceBlock nodes. Files are processed in parallel.
+  * */
+class AstCreationPass(filenames: List[String], cpg: Cpg, keyPool: IntervalKeyPool)
+    extends ParallelCpgPass[String](cpg, keyPools = Some(keyPool.split(filenames.size))) {
+
+  private val logger = LoggerFactory.getLogger(getClass)
+  val global: Global = Global()
+
+  override def partIterator: Iterator[String] = filenames.iterator
+
+  override def runOnPart(filename: String): Iterator[DiffGraph] = {
+
+    val diffGraph = DiffGraph.newBuilder
+    val absolutePath = new java.io.File(filename).toPath.toAbsolutePath.normalize().toString
+    val fileNode = nodes.NewFile(name = absolutePath)
+    diffGraph.addNode(fileNode)
+    val namespaceBlock = nodes.NewNamespaceBlock(
+      name = Defines.globalNamespaceName,
+      fullName = getGlobalNamespaceBlockFullName(Some(fileNode.name))
+    )
+    diffGraph.addNode(fileNode)
+    diffGraph.addNode(namespaceBlock)
+    diffGraph.addEdge(namespaceBlock, fileNode, EdgeTypes.SOURCE_FILE)
+
+    val driver = createDriver(fileNode, namespaceBlock)
+    tryToParse(driver, filename, diffGraph)
+  }
+
+  private def createDriver(fileNode: nodes.NewFile,
+                           namespaceBlock: nodes.NewNamespaceBlock): AntlrCModuleParserDriver = {
+    val driver = new AntlrCModuleParserDriver()
+    val astVisitor = new AstVisitor(driver, namespaceBlock, global)
+    driver.addObserver(astVisitor)
+    driver.setFileNode(fileNode)
+    driver
+  }
+
+  private def tryToParse(driver: AntlrCModuleParserDriver,
+                         filename: String,
+                         diffGraph: DiffGraph.Builder): Iterator[DiffGraph] = {
+    try {
+      driver.parseAndWalkFile(filename, diffGraph)
+      Iterator(diffGraph.build)
+    } catch {
+      case ex: RuntimeException => {
+        logger.warn("Cannot parse module: " + filename + ", skipping")
+        logger.warn("Complete exception: ", ex)
+        ex.printStackTrace()
+        Iterator()
+      }
+      case _: StackOverflowError => {
+        logger.warn("Cannot parse module: " + filename + ", skipping, StackOverflow")
+        Iterator()
+      }
+    }
+  }
+
+}
