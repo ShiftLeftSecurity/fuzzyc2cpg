3d7b80493dbca8aaacce773d1add9d1b212e9719
Fabian Yamaguchi <fabs@shiftleft.io>
2020-07-23 13:13:23 +0200

    Adapt CpgTestFixture and tests (locals have ORDER now)

--- src/test/scala/io/shiftleft/fuzzyc2cpg/CpgTestFixture.scala
+++ src/test/scala/io/shiftleft/fuzzyc2cpg/CpgTestFixture.scala
@@ -3,16 +3,24 @@ package io.shiftleft.fuzzyc2cpg
 import gremlin.scala.GraphAsScala
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.fuzzyc2cpg.output.inmemory.OutputModuleFactory
+import io.shiftleft.fuzzyc2cpg.passes.{AstCreationPass, CMetaDataPass, CfgCreationPass}
+import io.shiftleft.passes.IntervalKeyPool
+import io.shiftleft.semanticcpg.language._
 
 case class CpgTestFixture(projectName: String) {
 
-  val cpg: Cpg = {
-    val dirName = String.format("src/test/resources/testcode/%s", projectName)
-    val inmemoryOutputFactory = new OutputModuleFactory()
-    val fuzzyc2Cpg = new FuzzyC2Cpg(inmemoryOutputFactory)
-    fuzzyc2Cpg.runAndOutput(Set(dirName), Set(".c", ".cc", ".cpp", ".h", ".hpp"))
-    inmemoryOutputFactory.getInternalGraph
+  val cpg: Cpg = Cpg.emptyCpg
+  val dirName = String.format("src/test/resources/testcode/%s", projectName)
+  val keyPoolFile1 = new IntervalKeyPool(1001, 2000)
+  val cfgKeyPool = new IntervalKeyPool(2001, 3000)
+  val filenames = SourceFiles.determine(Set(dirName), Set(".c"))
+
+  new CMetaDataPass(cpg).createAndApply()
+  new AstCreationPass(filenames, cpg, keyPoolFile1).createAndApply()
+  if (cpg.method.size > 0) {
+    new CfgCreationPass(cpg, cfgKeyPool).createAndApply()
   }
+
 
   def V = cpg.graph.asScala.V
 
